31c8d3b3cd06f1737fac9b85e57d6b48266978cd
alisamettan <alisamet_tan@hotmail.com>
2024-02-07 12:10:25 +0300

    loading state yazıldı api den ürünler çekilip ekrana bastırıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get("https://fakestoreapi.com/products").then((res) => {
+      setProducts([...res.data]);
+      console.log(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <h1>Loading....</h1>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-row flex-wrap gap-10 justify-center items-center ">
+        {products.map((product) => {
+          return (
+            <div className="w-1/4 bg-slate-600 rounded-lg p-10">
+              <p>{product.description}</p>
+              <img className="w-56 h-64" src={product.image} alt="" />
+              <p>{product.price}</p>
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
